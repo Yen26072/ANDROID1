91d70a42287ec9a2751bfec054a4ef777b87d3a4
Yen2607 <Yen26072>
2025-05-16 18:01:57 +0700

    lan3

--- B1/app/src/main/java/com/example/ex1/MainActivity.java
+++ B1/app/src/main/java/com/example/ex1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editA, editB, editAdd;
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,20 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        editA = findViewById(R.id.editA);
+        editB = findViewById(R.id.editB);
+        editAdd = findViewById(R.id.editAdd);
+        btnAdd = findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(editA.getText().toString());
+                int b = Integer.parseInt(editB.getText().toString());
+                int c = a + b;
+                editAdd.setText(c+"");
+            }
         });
     }
 }
