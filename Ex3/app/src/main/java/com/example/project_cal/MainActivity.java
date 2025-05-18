2c39f8426a8d33d77abb661d31d26e2eb24b42e4
Yen2607 <Yen26072>
2025-05-18 22:19:43 +0700

    lan4

--- Ex3/app/src/main/java/com/example/project_cal/MainActivity.java
+++ Ex3/app/src/main/java/com/example/project_cal/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.project_cal;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edt1, edt2, edt3;
+    Button btncong, btntru, btnnhan, btnchia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,50 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        edt1 = findViewById(R.id.edta);
+        edt2 = findViewById(R.id.edtb);
+        edt3 = findViewById(R.id.edtc);
+        btncong = findViewById(R.id.btncong);
+        btntru = findViewById(R.id.btntru);
+        btnnhan = findViewById(R.id.btnnhan);
+        btnchia = findViewById(R.id.btnchia);
+        btncong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt("0" + edt1.getText());
+                int b = Integer.parseInt("0" + edt2.getText());
+                edt3.setText("a + b = " + (a+b));
+            }
+        });
+        btntru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt("0" + edt1.getText());
+                int b = Integer.parseInt("0" + edt2.getText());
+                edt3.setText("a - b = " + (a-b));
+            }
+        });
+        btnnhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt("0" + edt1.getText());
+                int b = Integer.parseInt("0" + edt2.getText());
+                edt3.setText("a * b = " + (a*b));
+            }
+        });
+        btnchia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt("0" + edt1.getText());
+                int b = Integer.parseInt("0" + edt2.getText());
+                if(b == 0){
+                    edt3.setText("b phải khác 0");
+                }
+                else{
+                    edt3.setText("a / b = " + (a/b));
+                }
+            }
         });
     }
 }
