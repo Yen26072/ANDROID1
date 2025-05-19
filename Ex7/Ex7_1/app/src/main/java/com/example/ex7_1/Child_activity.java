4e48cd5d7af7ca246bca9543e4a4557495da3a6e
Yen2607 <Yen26072>
2025-05-19 16:34:12 +0700

    lan4

--- Ex7/Ex7_1/app/src/main/java/com/example/ex7_1/Child_activity.java
+++ Ex7/Ex7_1/app/src/main/java/com/example/ex7_1/Child_activity.java
@@ -1,6 +1,10 @@
 package com.example.ex7_1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,15 @@ public class Child_activity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button btn = findViewById(R.id.button2);
+        TextView txt = (TextView) findViewById(R.id.textView2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(Child_activity.this, MainActivity.class);
+                startActivity(intent1);
+            }
         });
     }
 }
