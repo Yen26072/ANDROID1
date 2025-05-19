7dda13b261bb28f1e556cf4c726f73647bdc9fd3
Yen2607 <Yen26072>
2025-05-20 00:05:05 +0700

    lan4

--- Ex7/Ex7_3/app/src/main/java/com/example/ex7_3/MainActivity.java
+++ Ex7/Ex7_3/app/src/main/java/com/example/ex7_3/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.ex7_3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editA, editB, editKQ;
+    Button btnrequest;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 99 && resultCode == 33){
+            int sum = data.getIntExtra("kq", 0);
+            editKQ.setText("Tong 2 so la: " + sum);
+        }
+        if(requestCode == 99 && resultCode == 34){
+            int sub = data.getIntExtra("kq", 0);
+            editKQ.setText("Hieu 2 so la: "+sub);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +41,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        editA = findViewById(R.id.edita);
+        editB = findViewById(R.id.editb);
+        editKQ = findViewById(R.id.editkq);
+        btnrequest = findViewById(R.id.button);
+        btnrequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myintent = new Intent(MainActivity.this, ResultActivity.class);
+                int a = Integer.parseInt(editA.getText().toString());
+                int b = Integer.parseInt(editB.getText().toString());
+                myintent.putExtra("soa", a);
+                myintent.putExtra("sob", b);
+                startActivityForResult(myintent, 99);
+            }
+        });
+
     }
 }
--- Ex7/Ex7_3/app/src/main/java/com/example/ex7_3/ResultActivity.java
+++ Ex7/Ex7_3/app/src/main/java/com/example/ex7_3/ResultActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex7_3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ResultActivity extends AppCompatActivity {
 
+    EditText editAA, editBB;
+    Button btnsendtong, btnsendhieu;
+    Intent myintent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,33 @@ public class ResultActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        editAA = findViewById(R.id.editTextTextA);
+        editBB = findViewById(R.id.editTextTextB);
+        btnsendtong = findViewById(R.id.button2);
+        btnsendhieu = findViewById(R.id.button3);
+        myintent = getIntent();
+        int a = myintent.getIntExtra("soa", 0);
+        int b = myintent.getIntExtra("sob", 0);
+        editAA.setText(a+"");
+        editBB.setText(b+"");
+        btnsendtong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int sum = a+b;
+                myintent.putExtra("kq", sum);
+                setResult(33, myintent);
+                finish();
+            }
+        });
+        btnsendhieu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int sub = a-b;
+                myintent.putExtra("kq", sub);
+                setResult(34, myintent);
+                finish();
+            }
         });
     }
 }
