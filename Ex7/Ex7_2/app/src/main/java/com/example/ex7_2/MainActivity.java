52983221c73321b87cbd5989868699298248475e
Yen2607 <Yen26072>
2025-05-19 17:34:52 +0700

    lan2

--- Ex7/Ex7_2/app/src/main/java/com/example/ex7_2/MainActivity.java
+++ Ex7/Ex7_2/app/src/main/java/com/example/ex7_2/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex7_2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edita, editb;
+    Button btnkq;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        edita = findViewById(R.id.editTextText);
+        editb = findViewById(R.id.editTextText2);
+        btnkq = findViewById(R.id.button1);
+        btnkq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myintent = new Intent(MainActivity.this, ResultActivity.class);
+                Bundle bundle1 = new Bundle();
+                int a = Integer.parseInt(edita.getText()+"");
+                int b = Integer.parseInt(editb.getText()+"");
+                bundle1.putInt("soa", a);
+                bundle1.putInt("sob", b);
+                myintent.putExtra("mybackage", bundle1);
+                startActivity(myintent);
+            }
         });
     }
 }
