b6de4c716b820fa92024db288b34517692dadd40
Yen2607 <Yen26072>
2025-05-19 17:41:01 +0700

    lan3

--- Ex7/Ex7_2/app/src/main/java/com/example/ex7_2/ResultActivity.java
+++ Ex7/Ex7_2/app/src/main/java/com/example/ex7_2/ResultActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex7_2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class ResultActivity extends AppCompatActivity {
 
+    TextView txtkq;
+    Button btnback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,30 @@ public class ResultActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        txtkq = findViewById(R.id.txtkq);
+        btnback = findViewById(R.id.button);
+        Intent yourintent = getIntent();
+        Bundle yourbundle = yourintent.getBundleExtra("mybackage");
+        int a = yourbundle.getInt("soa");
+        int b = yourbundle.getInt("sob");
+        String kq="";
+        if(a==0 && b==0){
+            kq="Vo so nghiem";
+        }
+        else if(a==0 && b!=0){
+            kq="vo nghiem";
+        }
+        else {
+            DecimalFormat dcf = new DecimalFormat("0.##");
+            kq=dcf.format(-b*1.0/a);
+        }
+        txtkq.setText(kq+"");
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
