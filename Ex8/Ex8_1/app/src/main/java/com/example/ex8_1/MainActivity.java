ccddca34fc533053ae5a489c7c7c0017dcaa9523
Yen2607 <Yen26072>
2025-05-20 09:11:35 +0700

    lan3

--- Ex8/Ex8_1/app/src/main/java/com/example/ex8_1/MainActivity.java
+++ Ex8/Ex8_1/app/src/main/java/com/example/ex8_1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex8_1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btncall, btnsend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btncall = findViewById(R.id.btnCall);
+        btnsend = findViewById(R.id.btnSend);
+        btncall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, CallPhoneActivity.class);
+                startActivity(intent1);
+            }
+        });
+        btnsend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity.this, SendSMSActivity.class);
+                startActivity(intent2);
+            }
         });
     }
 }
