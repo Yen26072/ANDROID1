1468c328c42ea9dbcdc2072cec3b333b9ebfc234
Yen2607 <Yen26072>
2025-05-20 09:59:07 +0700

    lan4

--- Ex8/Ex8_1/app/src/main/java/com/example/ex8_1/CallPhoneActivity.java
+++ Ex8/Ex8_1/app/src/main/java/com/example/ex8_1/CallPhoneActivity.java
@@ -1,15 +1,27 @@
 package com.example.ex8_1;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CallPhoneActivity extends AppCompatActivity {
 
+    EditText editcall;
+    ImageButton btncallphone;
+    Button btnback1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +31,27 @@ public class CallPhoneActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        editcall = (EditText) findViewById(R.id.editCall);
+        btnback1 = (Button) findViewById(R.id.btnBack);
+        btncallphone = (ImageButton) findViewById(R.id.imageCall);
+        btncallphone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callintent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + editcall.getText().toString()));
+                if(ActivityCompat.checkSelfPermission(CallPhoneActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(CallPhoneActivity.this, new String[]{
+                            Manifest.permission.CALL_PHONE}, 1);
+                    return;
+                }
+                startActivity(callintent);
+            }
+        });
+        btnback1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
--- Ex8/Ex8_1/app/src/main/java/com/example/ex8_1/SendSMSActivity.java
+++ Ex8/Ex8_1/app/src/main/java/com/example/ex8_1/SendSMSActivity.java
@@ -1,6 +1,12 @@
 package com.example.ex8_1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +16,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SendSMSActivity extends AppCompatActivity {
 
+    EditText editsms;
+    Button btnback2;
+    ImageButton btnsendsms;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,22 @@ public class SendSMSActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        editsms = (EditText) findViewById(R.id.editsms);
+        btnback2 = (Button) findViewById(R.id.btnback2);
+        btnsendsms = (ImageButton) findViewById(R.id.imagesms);
+        btnsendsms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent smsintent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + editsms.getText().toString()));
+                startActivity(smsintent);
+            }
+        });
+        btnback2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
