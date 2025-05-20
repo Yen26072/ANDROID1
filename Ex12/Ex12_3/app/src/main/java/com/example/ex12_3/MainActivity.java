0327a3c03d0d37f145632f77266208bd9aa20c60
Yen2607 <Yen26072>
2025-05-20 22:02:15 +0700

    lan3

--- Ex12/Ex12_3/app/src/main/java/com/example/ex12_3/MainActivity.java
+++ Ex12/Ex12_3/app/src/main/java/com/example/ex12_3/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex12_3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    String arr[] = {"Hang dien tu", "Hang hoa chat", "Hang gia dung", "Hang xay dung"};
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        txt = (TextView) findViewById(R.id.txt1);
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item_single_choice);
+        spin.setAdapter(adapter);
+        spin.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                txt.setText(arr[position]);
+            }
         });
     }
 }
