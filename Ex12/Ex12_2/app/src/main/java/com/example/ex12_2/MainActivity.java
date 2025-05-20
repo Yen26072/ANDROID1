64d0a872f26d80a95f1f8297a45e7cc2590d8a68
Yen2607 <Yen26072>
2025-05-20 18:37:21 +0700

    lan3

--- Ex12/Ex12_2/app/src/main/java/com/example/ex12_2/MainActivity.java
+++ Ex12/Ex12_2/app/src/main/java/com/example/ex12_2/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.ex12_2;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
+    ListView lv;
+    ArrayList<String> arraywork;
+    ArrayAdapter<String> arrayAdapter;
+    EditText editwork, edithour, editminute;
+    TextView txtdate;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +38,43 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        edithour = findViewById(R.id.edithour);
+        editwork = findViewById(R.id.editwork);
+        editminute = findViewById(R.id.editminute);
+        txtdate = findViewById(R.id.textView2);
+        btn = findViewById(R.id.button);
+        lv = findViewById(R.id.listView);
+        arraywork = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arraywork);
+        lv.setAdapter(arrayAdapter);
+        Date currentDate = Calendar.getInstance().getTime();
+        java.text.SimpleDateFormat simpleDateFormat = new java.text.SimpleDateFormat("dd/MM/yyyy");
+        txtdate.setText("Hom nay: " + simpleDateFormat.format(currentDate));
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editwork.getText().toString().equals("")||editwork.getText().toString().equals("")||editminute.getText().toString().equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Info missing");
+                    builder.setMessage("Please enter all information of the work");
+                    builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    builder.show();
+                }
+                else {
+                    String str = editwork.getText().toString() + " - " + edithour.getText().toString() + ":" + editminute.getText().toString();
+                    arraywork.add(str);
+                    arrayAdapter.notifyDataSetChanged();
+                    edithour.setText("");
+                    editminute.setText("");
+                    editwork.setText("");
+                }
+            }
         });
     }
 }
