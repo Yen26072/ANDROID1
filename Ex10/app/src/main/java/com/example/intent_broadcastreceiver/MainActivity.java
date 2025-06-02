d432b04b8df947c501158938849015a9a972daae
Yen2607 <Yen26072>
2025-06-02 12:53:06 +0700

    lan4

--- Ex10/app/src/main/java/com/example/intent_broadcastreceiver/MainActivity.java
+++ Ex10/app/src/main/java/com/example/intent_broadcastreceiver/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.intent_broadcastreceiver;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +23,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS}, 1);
+            return;
+        }
     }
 }
