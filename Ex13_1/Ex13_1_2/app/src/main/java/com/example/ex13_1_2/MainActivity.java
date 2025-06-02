dd8484ca1d8c80df60a00149042511056040dacd
Yen2607 <Yen26072>
2025-06-02 22:11:33 +0700

    lan4

--- Ex13_1/Ex13_1_2/app/src/main/java/com/example/ex13_1_2/MainActivity.java
+++ Ex13_1/Ex13_1_2/app/src/main/java/com/example/ex13_1_2/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex13_1_2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    String arr[] = {"Ipad", "Iphone", "New Ipad", "SamSung", "Nokia", "Sony Ericson", "LG", "Q-Mobile", "HTC", "Blackberry", "G Phone", "FPT-Phone", "HK Phone"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,21 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        final TextView selection = (TextView) findViewById(R.id.selection);
+        final GridView gv = (GridView) findViewById(R.id.gridView);
+        ArrayAdapter<String>da=new ArrayAdapter<String>
+                (
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        arr
+                );
+        gv.setAdapter(da);
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selection.setText(arr[position]);
+            }
         });
     }
 }
