1e94a7edb419af20b2de65f8de2308c213051efb
Yen2607 <Yen26072>
2025-06-02 21:25:20 +0700

    lan3

--- Ex11/app/src/main/java/com/example/ex11/MainActivity.java
+++ Ex11/app/src/main/java/com/example/ex11/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex11;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnopen;
+    EditText edtlink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btnopen = findViewById(R.id.button);
+        edtlink = findViewById(R.id.editTextText);
+        btnopen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("http:/" + edtlink.getText().toString()));
+                startActivity(intent1);
+            }
         });
     }
 }
