744b035c7294cab6fe0c0c18911ff6a2b9d5bef3
Yen2607 <Yen26072>
2025-06-11 01:01:15 +0700

    lan2

--- Ex16/app/src/main/java/com/example/ex16/MainActivity.java
+++ Ex16/app/src/main/java/com/example/ex16/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex16;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edta, edtb, edtkq;
+    Button btntong, btnclear;
+    TextView txtlichsu;
+    String lichsu = "";
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences myprefs = getSharedPreferences("mysave", MODE_PRIVATE);
+        SharedPreferences.Editor myedit = myprefs.edit();
+        myedit.commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +37,33 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        edta = findViewById(R.id.edta);
+        edtb = findViewById(R.id.edtb);
+        edtkq = findViewById(R.id.edtkq);
+        btntong = findViewById(R.id.btntong);
+        btnclear = findViewById(R.id.btnclear);
+        txtlichsu = findViewById(R.id.txtlichsu);
+        SharedPreferences myprefs = getSharedPreferences("mysave", MODE_PRIVATE);
+        lichsu = myprefs.getString("ls", "");
+        txtlichsu.setText(lichsu);
+        btntong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(edta.getText().toString());
+                int b = Integer.parseInt(edtb.getText().toString());
+                int kq = a + b;
+                edtkq.setText(kq + "");
+                lichsu += a + " + " + b + " = " + kq;
+                txtlichsu.setText(lichsu);
+            }
+        });
+        btnclear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lichsu = "";
+                txtlichsu.setText(lichsu);
+            }
         });
     }
 }
