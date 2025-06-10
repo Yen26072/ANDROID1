bbea5e9eec16f089df048a4f2296c753ed75778a
Yen2607 <Yen26072>
2025-06-10 20:38:19 +0700

    lan2

--- Ex14/Ex14_1/app/src/main/java/com/example/ex14_1/MainActivity.java
+++ Ex14/Ex14_1/app/src/main/java/com/example/ex14_1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex14_1;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    EditText editA, editB;
+    Button btncong;
+    ListView lv;
+    ArrayList<String> list;
+    ArrayAdapter<String> myarray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +31,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        addControl();
+        addEvent();
     }
 }
