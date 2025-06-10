ddaee6b2eb30b19236d31ea90565f3a719cf4f64
Yen2607 <Yen26072>
2025-06-11 01:34:34 +0700

    lan2

--- Ex17/app/src/main/java/com/example/ex17/MainActivity.java
+++ Ex17/app/src/main/java/com/example/ex17/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex17;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    String DB_PATH_SUFFIX = "/database/";
+    SQLiteDatabase database=null;
+    String DATABASE_NAME = "qlsach.db";
+
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +35,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        processCopy();
+        database = openOrCreateDatabase("qlsach.db", MODE_PRIVATE, null);
+        lv = findViewById(R.id.lv);
+        mylist = new ArrayList<>();
+        myadapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, mylist);
+        lv.setAdapter(myadapter);
+        Cursor c = database.query("tbsach", null, null, null, null, null, null);
+        c.moveToNext();
+        String data = "";
+        while (c.isAfterLast() == false){
+            data = c.getString(0)+"-"+c.getString(1)+"-"+c.getString(2);
+            mylist.add(data);
+            c.moveToNext();
+        }
+        c.close();
+        myadapter.notifyDataSetChanged();
+    }
+
+    private void processCopy() {
+        File
     }
 }
