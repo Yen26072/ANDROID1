7e67d25bc922659ecb9309ca0eafe809ef631b1b
Yen2607 <Yen26072>
2025-06-10 20:55:34 +0700

    lan3

--- Ex14/Ex14_1/app/src/main/java/com/example/ex14_1/MainActivity.java
+++ Ex14/Ex14_1/app/src/main/java/com/example/ex14_1/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.ex14_1;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TabHost;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
@@ -33,5 +37,44 @@ public class MainActivity extends AppCompatActivity {
         });
         addControl();
         addEvent();
+    }
+
+    private void addEvent() {
+        btncong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Xulycong();
+            }
+
+            private void Xulycong() {
+                int a = Integer.parseInt(editA.getText().toString());
+                int b = Integer.parseInt(editB.getText().toString());
+                String c = a + " + " + b + " = " + (a+b);
+                list.add(c);
+                myarray.notifyDataSetChanged();
+                editA.setText("");
+                editB.setText("");
+            }
+        });
+    }
+
+    private void addControl() {
+        TabHost tab = (TabHost) findViewById(R.id.tabhost);
+        tab.setup();
+        TabHost.TabSpec tab1 = tab.newTabSpec("t1");
+        tab1.setContent(R.id.tab1);
+        tab1.setIndicator("", getResources().getDrawable(R.drawable.cong));
+        tab.addTab(tab1);
+        TabHost.TabSpec tab2 = tab.newTabSpec("t2");
+        tab1.setContent(R.id.tab2);
+        tab1.setIndicator("", getResources().getDrawable(R.drawable.lichsu));
+        tab.addTab(tab2);
+        editA = (EditText) findViewById(R.id.editTextText);
+        editB = (EditText) findViewById(R.id.editTextText2);
+        btncong = (Button) findViewById(R.id.button);
+        lv = (ListView) findViewById(R.id.lv);
+        list = new ArrayList<String>();
+        myarray = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, list);
+        lv.setAdapter(myarray);
     }
 }
