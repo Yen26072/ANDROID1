dbc7932666cf9154750780caed937570c49da9c2
Yen2607 <Yen26072>
2025-06-10 18:54:14 +0700

    lan5

--- Ex13_2_2/app/src/main/java/com/example/ex13_2_2/SubActivity.java
+++ Ex13_2_2/app/src/main/java/com/example/ex13_2_2/SubActivity.java
@@ -1,32 +1,25 @@
 package com.example.ex13_2_2;
 
-import android.annotation.SuppressLint;
+
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class SubActivity extends AppCompatActivity {
 
     private Bundle extra;
     TextView txtname2;
     ImageView img2;
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sub);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.gridView), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
         txtname2 = (TextView) findViewById(R.id.textView2);
         img2 = (ImageView) findViewById(R.id.imageView2);
         extra = getIntent().getExtras();
