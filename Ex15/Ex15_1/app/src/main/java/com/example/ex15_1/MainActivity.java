cf1243687e065dfc50776e3e92afa00d0fc40d5a
Yen2607 <Yen26072>
2025-06-11 00:20:03 +0700

    lan2

--- Ex15/Ex15_1/app/src/main/java/com/example/ex15_1/MainActivity.java
+++ Ex15/Ex15_1/app/src/main/java/com/example/ex15_1/MainActivity.java
@@ -1,6 +1,17 @@
 package com.example.ex15_1;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +19,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtmalop, edttenlop, edtsiso;
+    Button btninsert, btndelete, btnupdate, btnquery;
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
+    SQLiteDatabase mydatabase;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +40,74 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        edtmalop = findViewById(R.id.edtmalop);
+        edttenlop = findViewById(R.id.edttenlop);
+        edtsiso = findViewById(R.id.edtsiso);
+        btninsert = findViewById(R.id.btninsert);
+        btndelete = findViewById(R.id.btndelete);
+        btnupdate = findViewById(R.id.btnupdate);
+        btnquery = findViewById(R.id.btnquery);
+        lv = findViewById(R.id.lv);
+        mylist = new ArrayList<>();
+        myadapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mylist);
+        lv.setAdapter(myadapter);
+        mydatabase = openOrCreateDatabase("qlsinhvien.db", MODE_PRIVATE, null);
+        try{
+            String sql = "CREATE TABLE tbllop(malop TEXT primary key, tenlop TEXT, siso INTERGER)";
+            mydatabase.execSQL(sql);
+        }
+        catch (Exception e){
+            Log.e("Error", "Table da ton tai");
+        }
+        btninsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String malop = edtmalop.getText().toString();
+                String tenlop = edttenlop.getText().toString();
+                int siso = Integer.parseInt(edtsiso.getText().toString());
+                ContentValues myvalue = new ContentValues();
+                myvalue.put("malop", malop);
+                myvalue.put("tenlop", tenlop);
+                myvalue.put("siso", siso);
+                String msg = "";
+                if(mydatabase.insert("tbllop", null, myvalue) == -1){
+                    msg = "Fail to insert record!";
+                } else{
+                    msg = "Insert record sucessfully";
+                }
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+        btndelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String malop = edtmalop.getText().toString();
+                int n = mydatabase.delete("tbllop", "malop = ?", new String[]{malop});
+                String msg = "";
+                if(n == 0){
+                    msg = "No record to Delete";
+                } else {
+                    msg = n + "record is deleted";
+                }
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnquery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mylist.clear();
+                Cursor c = mydatabase.query("tbllop", null, null, null, null, null, null);
+                c.moveToNext();
+                String data = "";
+                while (c.isAfterLast() == false){
+                    data = c.getString(0) + " - " + c.getString(1) + " - " + c.getString(2);
+                    c.moveToNext();
+                    mylist.add(data);
+                }
+                c.close();
+                myadapter.notifyDataSetChanged();
+            }
+        });
+
     }
 }
