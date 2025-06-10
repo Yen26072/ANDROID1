685129e8ae69dc24a0bca8a260d95552a0bd47f4
Yen2607 <Yen26072>
2025-06-11 01:52:46 +0700

    lan3

--- Ex17/app/src/main/java/com/example/ex17/MainActivity.java
+++ Ex17/app/src/main/java/com/example/ex17/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.ex17;
 
+import static java.nio.file.Files.exists;
+
 import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -54,6 +62,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void processCopy() {
-        File
+        File dbFile = getDatabasePath(DATABASE_NAME);
+        if(!dbFile.exists()){
+            try{
+                CopyDataBaseFromAsset();
+                Toast.makeText(this, "Copy sucess from Assets folder", Toast.LENGTH_LONG).show();
+                }
+            catch (Exception e){
+                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+                }
+        }
+    }
+
+    private  String getDatabasePath(){
+        return getApplicationInfo().dataDir + DB_PATH_SUFFIX + DATABASE_NAME;
+    }
+
+    private void CopyDataBaseFromAsset() {
+        try{
+            InputStream myInput;
+            myInput = getAssets().open(DATABASE_NAME);
+            String outFileName = getDatabasePath();
+            File f = new File(getApplicationInfo().dataDir + DB_PATH_SUFFIX);
+            if(!f.exists()){
+                f.mkdir();
+                OutputStream myOutput = new FileOutputStream(outFileName);
+                int size = myInput.available();
+                byte[] buffer = new byte[size];
+                myInput.read(buffer);
+                myOutput.write(buffer);
+                myOutput.flush();
+                myOutput.close();
+                myInput.close();
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
