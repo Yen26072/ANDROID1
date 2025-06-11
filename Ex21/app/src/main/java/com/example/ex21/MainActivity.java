cb8c878160c1721516107517e1e1616e453ff8ff
Yen2607 <Yen26072>
2025-06-12 02:01:49 +0700

    lan1

--- Ex21/app/src/main/java/com/example/ex21/MainActivity.java
+++ Ex21/app/src/main/java/com/example/ex21/MainActivity.java
@@ -1,77 +1,61 @@
 package com.example.ex21;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.view.View;
+
+import com.example.ex21.R;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnparse;
+    Button btnParse;
     ListView lv;
-    ArrayList<String> mylist;
-    ArrayAdapter myadapter;
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        btnparse = findViewById(R.id.btnparse);
+
+        btnParse = findViewById(R.id.btnparse);
         lv = findViewById(R.id.lv);
-        mylist = new ArrayList<>();
-        myadapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mylist);
-        lv.setAdapter(myadapter);
-        btnparse.setOnClickListener(new View.OnClickListener() {
+        list = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        lv.setAdapter(adapter);
+
+        btnParse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                parsejson();
-            }
-
-            private void parsejson() {
-                String json = null;
-                try{
+                try {
                     InputStream inputStream = getAssets().open("computer.json");
                     int size = inputStream.available();
                     byte[] buffer = new byte[size];
+                    inputStream.read(buffer);
                     inputStream.close();
-                    json = new String(buffer, "UTF-8");
-                    JSONObject reader = new JSONObject(json);
-                    mylist.add(reader.getString("MaDM"));
-                    mylist.add(reader.getString("TenMD"));
-                    JSONArray myarray = reader.getJSONArray("Sanphams");
-                    for(int i=0; i<myarray.length(); i++){
-                        JSONObject myobj = myarray.getJSONObject(i);
-                        mylist.add(myobj.getString("MaSP")+" - "+myobj.getString("TenSP"));
-                        mylist.add(myobj.getString("SoLuong")+" * "+myobj.getString("DonGia")+" = "+myobj.getString("ThanhTien"));
-                        mylist.add(myobj.getString("Hinh"));
+                    String json = new String(buffer, "UTF-8");
+
+                    JSONObject obj = new JSONObject(json);
+                    JSONArray sanphams = obj.getJSONArray("Sanphams");
+
+                    list.clear();
+                    for (int i = 0; i < sanphams.length(); i++) {
+                        JSONObject sp = sanphams.getJSONObject(i);
+                        list.add(sp.getString("TenSP"));
                     }
-                    myadapter.notifyDataSetChanged();
-                }
-                catch (IOException e1){
-                    e1.printStackTrace();
-                }
-                catch (JSONException e2){
-                    e2.printStackTrace();
+
+                    adapter.notifyDataSetChanged();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         });
