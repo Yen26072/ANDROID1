7b2dc3212e0517c867ca79f293859084e358f527
Yen2607 <Yen26072>
2025-06-12 15:32:04 +0700

    lan2

--- Ex24/app/src/main/java/com/example/ex24/MyArrayAdapter.java
+++ Ex24/app/src/main/java/com/example/ex24/MyArrayAdapter.java
@@ -1,4 +1,52 @@
 package com.example.ex24;
 
-public class MyArrayAdapter {
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class MyArrayAdapter extends ArrayAdapter<List> {
+    private Activity context;
+    private ArrayList<List> arr;
+    private int layoutID;
+
+    public MyArrayAdapter(Activity context, int layoutID, ArrayList<List> arr) {
+        super(context, layoutID, arr);
+        this.context = context;
+        this.arr = arr;
+        this.layoutID = layoutID;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        convertView = inflater.inflate(layoutID, null);
+        final List lst = arr.get(position);
+        ImageView imgitem = convertView.findViewById(R.id.imageView);
+        imgitem.setImageBitmap(lst.getImg());
+        TextView txtTitle = convertView.findViewById(R.id.txtTitle);
+        txtTitle.setText(lst.getTitle().toString());
+        TextView txtInfo = convertView.findViewById(R.id.txtInfo);
+        txtInfo.setText(lst.getInfo().toString());
+        MainActivity.lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(lst.getLink()));
+                context.startActivity(intent);
+            }
+        });
+        return convertView;
+    }
 }
--- Ex24/app/src/main/java/com/example/ex24/XMLParser.java
+++ Ex24/app/src/main/java/com/example/ex24/XMLParser.java
@@ -1,4 +1,38 @@
 package com.example.ex24;
 
+import android.support.v4.app.INotificationSideChannel;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.w3c.dom.Entity;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.sql.ClientInfoStatus;
+
 public class XMLParser {
+    public String getXmlFromUrl(String url){
+        String xml = null;
+        try{
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            xml = EntityUtils.toString(httpEntity);
+        }
+        catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        catch (ClientProtocolException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return xml;
+    }
 }
