6ef513ad962184ae3a5055c67b70a852e8621d94
Yen2607 <Yen26072>
2025-06-12 12:37:17 +0700

    lan3

--- Ex22/app/src/main/java/com/example/ex22/MainActivity.java
+++ Ex22/app/src/main/java/com/example/ex22/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex22;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.channels.AsynchronousChannelGroup;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    Button btnnparse;
+    ListView lv;
+    ArrayAdapter<String> myadapter;
+    ArrayList<String> mylist;
+    String URL = "http://api.androidhive.info/pizza/?format=xml";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +40,87 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnnparse = (Button) findViewById(R.id.button);
+        lv = (ListView) findViewById(R.id.lv);
+        mylist = new ArrayList<>();
+        myadapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, mylist);
+        lv.setAdapter(myadapter);
+        btnnparse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoadExampleTask task = new LoadExampleTask();
+                task.execute();
+            }
+        });
+    }
+    class LoadExampleTask extends AsyncTask<Void, Void, ArrayList<String>> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            myadapter.clear();
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> strings) {
+            super.onPostExecute(strings);
+            myadapter.clear();
+            myadapter.addAll(strings);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected ArrayList<String> doInBackground(Void... voids) {
+            ArrayList<String> list = new ArrayList<String>();
+            try{
+                XmlPullParserFactory fc = XmlPullParserFactory.newInstance();
+                XmlPullParser parser = fc.newPullParser();
+                XMLParser myparser = new XMLParser();
+                String xml = myparser.getXmlFromUrl(URL);
+                parser.setInput(new StringReader(xml));
+                int eventType = -1;
+                String nodeName;
+                String datashow = "";
+                while (eventType!=XmlPullParser.END_DOCUMENT){
+                    eventType = parser.next();
+                    switch (eventType){
+                        case  XmlPullParser.START_DOCUMENT:
+                            break;
+                        case XmlPullParser.END_DOCUMENT:
+                            break;
+                        case XmlPullParser.START_TAG:
+                            nodeName = parser.getName();
+                            if(nodeName.equals("id")){
+                                datashow+=parser.nextText()+"-";
+                            }
+                            else if(nodeName.equals("name")){
+                                datashow+=parser.nextText();
+                            }
+                            break;
+                        case XmlPullParser.END_TAG:
+                            nodeName = parser.getName();
+                            if(nodeName.equals("item")){
+                                list.add(datashow);
+                                datashow = "";
+                            }
+                            break;
+                    }
+                }
+            }
+            catch (XmlPullParserException e){
+                e.printStackTrace();
+            }
+            catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
